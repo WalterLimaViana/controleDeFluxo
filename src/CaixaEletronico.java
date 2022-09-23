7bfde5fea675729643d4cd6f0ab493ea6984f44c
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-22 22:25:40 -0300

    changed method using if else to show values

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,12 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 27.0;
 
         if(valorSolicitado < saldo ){
             saldo = saldo - valorSolicitado;
 
             System.out.println("R$ " + saldo);
+        }
+        else{
+            System.out.println("Saldo Insuficiente. Saldo Atual: R$ " + saldo + ". Valor Solicitado: R$ "  + valorSolicitado);
         }
     }
 }
