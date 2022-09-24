9f7a72fb5048352d5e41e1559fdc4b5e8de679f7
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-23 21:49:44 -0300

    updated Resultado escolar using conditional (ternary)

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -2,17 +2,23 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota >= 7){
-            System.out.println("Aprovado");
-        }
+        // Condição Encadeada
+        // if(nota >= 7){
+        //     System.out.println("Aprovado");
+        // }
 
-        else if( nota >= 5 && nota < 7 ){
-            System.out.println("Prova de recuperação");
-        }
+        // else if( nota >= 5 && nota < 7 ){
+        //     System.out.println("Prova de recuperação");
+        // }
 
-        else{
-            System.out.println("Reprovado");
-        }
+        // else{
+        //     System.out.println("Reprovado");
+        // }
+
+        //Condição Ternária
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
 
     }
 
