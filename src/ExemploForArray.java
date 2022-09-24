d0f83a1af3022be0a530f119691026428a6636d5
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-23 23:48:39 -0300

    using o forEach in array

--- src/ExemploForArray.java
+++ src/ExemploForArray.java
@@ -4,6 +4,11 @@ public class ExemploForArray {
         for(int i=0; i < alunos.length; i++) {
             System.out.println("O aluno no índice i = " + i + " é o " + alunos[i]);
         }
+        //Usando o forEach
+        String alunos2 [] ={ "Felipe", "Marcos","Daniel", "Pedro"};
+        for(String aluno : alunos2){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
     }
     
 }
