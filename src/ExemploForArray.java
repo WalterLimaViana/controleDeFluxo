d3692bdb684c5e2cc558ca77c91104ba3a0952c1
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-23 23:40:04 -0300

    created examples using for

---
+++ src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] ={ "Felipe", "Marcos","Daniel", "Pedro"};
+        for(int i=0; i < alunos.length; i++) {
+            System.out.println("O aluno no índice i = " + i + " é o " + alunos[i]);
+        }
+    }
+    
+}
