ef496ce523ffa8bb03f04ec88cee3043a1a26dae
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-24 22:40:36 -0300

    using while an example with candies

---
+++ src/ExemploWhile.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while( mesada > 0){
+            Double valorDoce = valorAleatório();
+
+            if( valorDoce > mesada)
+                valorDoce = mesada;
+                System.out.println("Valor do doce: " + valorDoce + " Adicionado ao carrinho");
+                mesada = mesada - valorDoce;
+            
+            }
+            System.out.println("Mesada: " + mesada);
+            System.out.println("Joãozinho gastou toda a sua mesada em doces");
+
+    }    
+    private static double valorAleatório(){
+        return ThreadLocalRandom.current().nextDouble(2,8);
+    }
+}
