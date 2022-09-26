db88f8f8f4e85aefdf4d4dc098397601819f795c
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-25 22:58:24 -0300

    using try catch

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,9 +1,13 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    try{
+    
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
     System.out.println("Digite o seu nome");
     String nome = scanner.next();
@@ -18,6 +22,11 @@ public class AboutMe {
     System.out.println("Tenho " + idade + " anos");
     System.out.println("Minha altura é " + altura + "cm");
     scanner.close();
+    }
+
+    catch(InputMismatchException e){
+        System.out.println("Os campos idade e altura precisam ser numéricos!");
+    }
 
     }
 }
